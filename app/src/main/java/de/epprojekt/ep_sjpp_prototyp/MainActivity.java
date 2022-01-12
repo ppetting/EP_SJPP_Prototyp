9ecf68e55c456ee6ac11a91e89a875dbf99be55f
ppetting <patrick_alexander.pettinger@smail.th-koeln.de>
2022-01-12 16:12:58 +0100

    toolbar removed

--- app/src/main/java/de/epprojekt/ep_sjpp_prototyp/MainActivity.java
+++ app/src/main/java/de/epprojekt/ep_sjpp_prototyp/MainActivity.java
@@ -1,6 +1,8 @@
 package de.epprojekt.ep_sjpp_prototyp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
