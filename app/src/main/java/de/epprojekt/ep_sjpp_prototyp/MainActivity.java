826cf7008a55f34001f95c129c59e36500886158
ppetting <patrick_alexander.pettinger@smail.th-koeln.de>
2022-01-14 14:54:19 +0100

    warenkorb added

--- app/src/main/java/de/epprojekt/ep_sjpp_prototyp/MainActivity.java
+++ app/src/main/java/de/epprojekt/ep_sjpp_prototyp/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageButton;
 
 
