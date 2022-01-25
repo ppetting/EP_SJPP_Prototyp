a55069eec0c413bb59c2f648a007d5f2e3f20f3a
ppetting <patrick_alexander.pettinger@smail.th-koeln.de>
2022-01-25 14:09:08 +0100

    sound added menue activity

--- app/src/main/java/de/epprojekt/ep_sjpp_prototyp/MainActivity.java
+++ app/src/main/java/de/epprojekt/ep_sjpp_prototyp/MainActivity.java
@@ -3,12 +3,15 @@ package de.epprojekt.ep_sjpp_prototyp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton ibtnGemueseUndObst, ibtnGetraenke, ibtnWarenkorb, ibtnHome;
+    ImageButton ibtnGemueseUndObst, ibtnGetraenke, ibtnWarenkorb, ibtnMenue;
+    ImageButton ibtnSoundGetraenke;
     DBHelferlein hilfMirDaddyDB;
 
     @Override
@@ -19,17 +22,27 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbarMAIN);
         setSupportActionBar(toolbar);
 
-        ibtnHome = findViewById(R.id.imageButtonHome);
-        ibtnWarenkorb = findViewById(R.id.imageButtonWarenkorb);
-        ibtnGemueseUndObst = findViewById(R.id.imageButtonGemueseUndObst);
-        ibtnGetraenke = findViewById(R.id.imageButtonGetraenke);
+        hilfMirDaddyDB = new DBHelferlein(this);
 
-        ibtnHome.setImageResource(R.drawable.home);
-        ibtnGetraenke.setImageResource(R.drawable.getraenke);
-        ibtnGemueseUndObst.setImageResource(R.drawable.gemueseundobst);
+        ibtnMenue = findViewById(R.id.imageButtonHome);
+        ibtnMenue.setImageResource(R.drawable.menue);
+
+        ibtnWarenkorb = findViewById(R.id.imageButtonWarenkorb);
         ibtnWarenkorb.setImageResource(R.drawable.warenkorb);
 
-        hilfMirDaddyDB = new DBHelferlein(this);
+        ibtnGemueseUndObst = findViewById(R.id.imageButtonGemueseUndObst);
+        ibtnGemueseUndObst.setImageResource(R.drawable.gemueseundobst);
+
+        ibtnGetraenke = findViewById(R.id.imageButtonGetraenke);
+        ibtnGetraenke.setImageResource(R.drawable.getraenke);
+
+        ibtnSoundGetraenke = findViewById(R.id.imageButtonSoundGetraenke);
+        ibtnSoundGetraenke.setImageResource(R.drawable.refresh);
+
+        //SOUND
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.getraenke);
+        ibtnSoundGetraenke.setOnClickListener(v ->
+                mediaPlayer.start());
 
         ibtnGemueseUndObst.setOnClickListener(v -> {
             Intent intentGemueseUndObst = new Intent(MainActivity.this,GemueseUndObstActivity.class);
@@ -46,9 +59,13 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intentWarenkorb);
         });
 
-
+        ibtnMenue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMenue = new Intent(MainActivity.this, MenueActivity.class);
+                startActivity(intentMenue);
+            }
+        });
 
     }
-
-
 }
---
+++ app/src/main/java/de/epprojekt/ep_sjpp_prototyp/MenueActivity.java
@@ -0,0 +1,14 @@
+package de.epprojekt.ep_sjpp_prototyp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menue);
+    }
+}
