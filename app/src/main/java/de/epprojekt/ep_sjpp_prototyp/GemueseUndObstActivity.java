6315a4f3c1c5ea8bae17d1b325b5de2eeaee1111
ppetting <patrick_alexander.pettinger@smail.th-koeln.de>
2022-02-04 17:59:13 +0100

    user können individuel bilder setzten

--- app/src/main/java/de/epprojekt/ep_sjpp_prototyp/GemueseUndObstActivity.java
+++ app/src/main/java/de/epprojekt/ep_sjpp_prototyp/GemueseUndObstActivity.java
@@ -1,13 +1,27 @@
 package de.epprojekt.ep_sjpp_prototyp;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.net.URI;
 
 public class GemueseUndObstActivity extends AppCompatActivity {
 
-    ImageButton ibtnApfel, ibtnGurke;
+    ImageButton ibtnApfel, ibtnGurke, ibtnWarenkorb;
+    DBHelferlein hilfMirDaddyDB;
+    AnimationsHelferlein hilfMirMommyAnimation;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +30,37 @@ public class GemueseUndObstActivity extends AppCompatActivity {
 
         ibtnApfel = findViewById(R.id.imageButtonApfel);
         ibtnGurke = findViewById(R.id.imageButtonGurke);
+        imageView = findViewById(R.id.IVTEST);
 
-        ibtnApfel.setImageResource(R.drawable.apfel);
-        ibtnGurke.setImageResource(R.drawable.gurke);
 
+        ibtnApfel.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            activityResultLauncher.launch(intent);
+        });
     }
 
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult activityResult) {
+            int result = activityResult.getResultCode();
+            Intent data = activityResult.getData();
 
+            if (result == RESULT_OK) {
+                Uri selectedImageUri = data.getData();
+                ibtnGurke.setImageURI(selectedImageUri);
+            }
+        }
+
+    });
 }
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/de/epprojekt/ep_sjpp_prototyp/PictureChangeActivity.java
@@ -0,0 +1,14 @@
+package de.epprojekt.ep_sjpp_prototyp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PictureChangeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_change);
+    }
+}
